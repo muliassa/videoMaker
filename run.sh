f2e7f3e6bf49464b9b309a722b4fe8a5ec8205be
Muli Assa <muliassa@gmail.com>
2026-02-03 22:30:24 +0200

    run sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=/usr/local/cuda-11.8/targets/x86_64-linux/lib:/home/surfai/dev/clipHelper/downloads/onnxruntime-linux-x64-gpu-1.18.0/lib:$LD_LIBRARY_PATH
+export LD_PRELOAD=/usr/local/cuda-11.8/targets/x86_64-linux/lib/libcublasLt.so.11.11.3.6:/usr/local/cuda-11.8/targets/x86_64-linux/lib/libcublas.so.11.11.3.6
+exec build/clips $@
